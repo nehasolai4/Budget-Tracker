9865a2b8d0298f7dda229bd5a03022d058b92c45
Neha <nehasolai@gmail.com>
2025-12-29 18:41:21 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,106 @@
+let form = document.getElementById('transactionForm')
+let lst = document.getElementById('transactionList')
+
+let transactions=[]
+
+let income = document.getElementById('income')
+let expense = document.getElementById('expense')
+let balance = document.getElementById('balance')
+
+
+form.addEventListener('submit',function(event){
+    event.preventDefault()
+    console.log("form submitted!!")
+
+    let type = document.getElementById('type').value
+    let category = document.getElementById('category').value
+    let amt = document.getElementById('amount').value
+    let date = document.getElementById('date').value
+
+    amt = Number(amt)
+
+    const transaction={
+        id: Date.now(),
+        type: type,
+        category: category,
+        amount: amt,
+        date: date
+    }
+
+    transactions.push(transaction)
+    saveTransactions();
+    renderTransactions();
+    updateSummary();
+
+    
+});
+
+lst.addEventListener("click",function(event){
+    if(event.target.tagName==="BUTTON"){
+        let reqId=Number(event.target.dataset.id)
+        transactions=transactions.filter(t=>t.id!=reqId)
+
+        saveTransactions()
+        renderTransactions()
+        updateSummary()
+    }
+});
+
+function renderTransactions(){
+    lst.innerHTML = "";
+    for(let i=0; i<transactions.length; i++){
+        let li = document.createElement("li")
+        let span = document.createElement("span")
+        span.textContent=`${transactions[i].category}-${transactions[i].amount}`
+        span.className=transactions[i].type
+
+        li.appendChild(span)
+        
+
+        let delBtn = document.createElement("button")
+        delBtn.textContent="Delete" 
+        delBtn.dataset.id=transactions[i].id
+
+        li.appendChild(delBtn)
+        lst.appendChild(li)
+
+    }
+}
+
+
+function updateSummary(){
+    let totalIncome=0
+    let totalExpense=0
+
+    for(let i=0;i<transactions.length;i++){
+        if(transactions[i].type==='income'){
+            totalIncome+=transactions[i].amount;
+        }
+
+        else{
+            totalExpense+=transactions[i].amount;
+        }
+    }
+
+    totalBalance=totalIncome-totalExpense
+
+    income.textContent=totalIncome
+    expense.textContent=totalExpense
+    balance.textContent=totalBalance
+}
+
+
+function saveTransactions(){
+    localStorage.setItem("transactions",JSON.stringify(transactions))
+}
+
+function loadTransactions(){
+    let data = localStorage.getItem("transactions")
+    if(data){
+        transactions=JSON.parse(data)
+        renderTransactions()
+        updateSummary()
+    }
+}
+
+loadTransactions()
